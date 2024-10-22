f46177d37d679ac10bd92035d5a2d5fcb0004dd0
Sonhs1126 <thsgustn1126@naver.com>
2024-10-22 10:34:59 +0900

    웹후크 테스트3

--- Already/src/already.java
+++ Already/src/already.java
@@ -4,18 +4,6 @@ public class already {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		System.out.println("codebamer");
-		
-		System.out.println("codebamer");
-		System.out.println("codebamer");
 	}
 
 }
